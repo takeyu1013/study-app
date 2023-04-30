b73610ca153556a9da856a488fc265d003eb3779
takeyu1013 <yuto1013@gmail.com>
2023-04-30 13:03:31 +0900

    💄

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,29 @@
 import { Flex, Title } from "@tremor/react";
 import Search from "./_component/Search";
 import Image from "next/image";
+import { ScrollArea } from "./_component/ui/scroll-area";
 
 export default function Home() {
   return (
     <main className="p-2 bg-neutral-100 h-screen">
       <Search />
       <Title>注目のシェフ</Title>
-      <Flex justifyContent="start" className="gap-4">
-        <Image
-          alt="image"
-          src="https://placekitten.com/150/250"
-          width={150}
-          height={250}
-          className="rounded-2xl"
-        />
-        <Image
-          alt="image"
-          src="https://placekitten.com/150/250"
-          width={150}
-          height={250}
-          className="rounded-2xl"
-        />
-      </Flex>
+      <ScrollArea>
+        <Flex justifyContent="start" className="w-[1608px] gap-3">
+          {[...Array(10)].map((_, index) => {
+            return (
+              <Image
+                key={index}
+                alt="image"
+                src="https://placekitten.com/150/250"
+                width={150}
+                height={250}
+                className="rounded-2xl"
+              />
+            );
+          })}
+        </Flex>
+      </ScrollArea>
       <Title>話題のレシピ</Title>
       <Title>シェフ</Title>
     </main>

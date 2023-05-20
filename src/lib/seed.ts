08a7155a50f932c385bdba4c1c23d2fa089d4765
takeyu1013 <yuto1013@gmail.com>
2023-05-20 19:36:25 +0900

    🌱

--- src/lib/seed.ts
+++ src/lib/seed.ts
@@ -1,18 +1,14 @@
-import { drizzle } from "drizzle-orm/better-sqlite3";
-import { migrate } from "drizzle-orm/better-sqlite3/migrator";
-import Database from "better-sqlite3";
-import { unlink } from "fs";
-import { users } from "./schema";
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/libsql";
+import { migrate } from "drizzle-orm/libsql/migrator";
 
-unlink("local.db", (error) => {
-  if (error) console.error(error);
-});
-const sqlite = new Database("local.db");
-const db = drizzle(sqlite);
-migrate(db, {
-  migrationsFolder: "drizzle",
-});
+import { DATABASE_AUTH_TOKEN, DATABASE_URL } from "./env";
+import { createClient } from "@libsql/client";
 
-db.insert(users).values({ name: "Andrew" }).run();
-const allUsers = db.select().from(users).all();
-console.log("allUsers", allUsers);
+const client = createClient({
+  url: DATABASE_URL,
+  authToken: DATABASE_AUTH_TOKEN,
+});
+export const db = drizzle(client);
+
+migrate(db, { migrationsFolder: "drizzle" });

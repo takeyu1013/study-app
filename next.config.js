3170b63c750f8662f031df187c7e35c8186566fb
takeyu1013 <yuto1013@gmail.com>
2023-05-21 13:17:32 +0900

    🔧

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
-    serverComponentsExternalPackages: ["@tremor/react", "better-sqlite3"],
+    serverComponentsExternalPackages: ["@tremor/react"],
   },
   images: {
     remotePatterns: [

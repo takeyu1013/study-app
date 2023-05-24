3f0a4691f7d82d524886e58c26e4182b1b42cbf5
takeyu1013 <yuto1013@gmail.com>
2023-05-24 22:29:24 +0900

    🛂

--- src/app/_component/login.tsx
+++ src/app/_component/login.tsx
@@ -1,13 +1,17 @@
 "use client";
+import { useSearchParams } from "next/navigation";
 import { signIn } from "next-auth/react";
 
 import { Button } from "./button";
 
 export const Login = () => {
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl") || "/dashboard";
+
   return (
     <Button
       onClick={() => {
-        signIn("github", { callbackUrl: "/dashboard" });
+        signIn("github", { callbackUrl });
       }}
     >
       GitHubでログイン

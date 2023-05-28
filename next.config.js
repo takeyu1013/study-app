a35031b8d95290223ca996606b0a8904ef1bb9fc
takeyu1013 <yuto1013@gmail.com>
2023-05-28 15:57:47 +0900

    🔧

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
+  experimental: { typedRoutes: true },
   experimental: {
     serverComponentsExternalPackages: ["@tremor/react"],
   },

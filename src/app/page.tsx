7f64fa7b65da61933caf5824d22e19f759ace24e
takeyu1013 <yuto1013@gmail.com>
2023-06-04 09:21:02 +0900

    🛂

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import Link from "next/link";
+
+import { Button } from "@/app/_component/button";
+
 import { Login } from "./_component/login";
 
 export default function Home() {
   return (
-    <main className="flex h-screen items-center justify-center">
+    <main className="flex gap-2 h-screen items-center justify-center">
       <Login />
+      <Button variant="outline">
+        <Link href={"/dashboard"}>ログインせずに始める</Link>
+      </Button>
     </main>
   );
 }

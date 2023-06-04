c67892bc2c762a57fee910779e40b90b012af32c
takeyu1013 <yuto1013@gmail.com>
2023-06-04 15:37:11 +0900

    🚧

--- src/app/dashboard/search-section.tsx
+++ src/app/dashboard/search-section.tsx
@@ -1,48 +1,59 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useRef, useState, useTransition } from "react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
-import { useDebouncedValue } from "@mantine/hooks";
-
 import { Search } from "@/app/dashboard/search";
+
+function List({ q }: { q: string }) {
+  return (
+    <ul>
+      {["aaa", "bbb", "ccc", "ddd"]
+        .filter((string) => {
+          if (q === "") return true;
+          return string.includes(q);
+        })
+        .map((string, index) => {
+          return <li key={index}>{string}</li>;
+        })}
+    </ul>
+  );
+}
 
 export default function SearchSection() {
   const pathname = usePathname();
   const searchParams = useSearchParams()!;
   const { push } = useRouter();
-  const [value, setValue] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const [debaoucedValue] = useDebouncedValue(value, 1000);
-
-  useEffect(() => {
-    console.log("call API", debaoucedValue);
-    setIsLoading(false);
-    const param = debaoucedValue.trim();
-    if (param === "") {
-      push(pathname);
-      return;
-    }
-    const params = new URLSearchParams(searchParams.toString());
-    params.set("q", param);
-    push(pathname + "?" + params.toString());
-  }, [debaoucedValue, searchParams, pathname, push]);
+  const [isPending, startTransition] = useTransition();
+  const inputRef = useRef<HTMLInputElement>(null);
 
   return (
     <>
       <Search
         type="search"
         placeholder="シェフやレシピを検索"
-        value={value}
         onChange={(event) => {
-          setIsLoading(true);
-          const {
-            target: { value },
-          } = event;
-          setValue(value);
+          startTransition(() => {
+            setTimeout(() => {
+              const {
+                target: { value },
+              } = event;
+              const param = value.trim();
+              console.log("param", param);
+              if (param === "") {
+                push(pathname);
+                return;
+              }
+              const params = new URLSearchParams(searchParams.toString());
+              params.set("q", param);
+              push(pathname + "?" + params.toString());
+            }, 1000);
+          });
         }}
+        ref={inputRef}
       />
-      {isLoading && <div>Loading...</div>}
+      {isPending && <div>Loading...</div>}
+      <List q={searchParams.get("q") || ""} />
     </>
   );
 }
